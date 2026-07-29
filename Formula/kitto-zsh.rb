f6517a5a0b0854eca6652bb7b5b25303d2701b04
Matt Pilott <matt@pilott.me>
2026-07-29 12:02:03 +0100

    kitto-zsh 0.4.0

--- Formula/kitto-zsh.rb
+++ Formula/kitto-zsh.rb
@@ -1,8 +1,8 @@
 class KittoZsh < Formula
   desc "Backpack of zsh helpers for Mac JS development"
   homepage "https://github.com/mattpilott/kitto-zsh"
-  url "https://github.com/mattpilott/kitto-zsh/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "ec23fcdf3483edc11ac18e7ba706b44c8b06e89cb649d79a673461033f54b37f"
+  url "https://github.com/mattpilott/kitto-zsh/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "0212a499b0a917b9fe411c6ccab92dedd36c092f2eaea446ad204779c0c6a543"
   license "MIT"
 
   def install
